a39ba13afd7cd78cb2858f3e7f5e5b1629010000
Igor Khrypak <igor@temy.co>
2020-03-30 18:54:27 +0300

    Add basic interface logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import './App.css';
+import * as Converter from './helpers/converter.helper'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Roman-integer converter
-      </header>
-      <div className="Content-form">
-        <input type="text" placeholder="type value, ex. 26, XVII" />
-        <button>Convert to integer</button>
-        <button>Convert to roman</button>
-        <span>result</span>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {input: '', result: ''}
+  }
+  convertToInteger() {
+    console.log(this.state);
+    const result = Converter.toInteger(this.state.input);
+    this.setState({result});
+  }
+
+  convertToRoman() {
+    console.log(this.state);
+    const result = Converter.toRoman(this.state.input);
+    this.setState({result});
+  }
+
+  handleChange(e) {
+    const input = e.target.value;
+    this.setState({input});
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Roman-integer converter
+        </header>
+        <div className="Content-form">
+          <input value={this.state.input} onChange={(e)=> this.handleChange(e)} type="text" placeholder="type value, ex. 26, XVII" />
+          <button onClick={this.convertToInteger.bind(this)}>Convert to integer</button>
+          <button onClick={this.convertToRoman.bind(this)}>Convert to roman</button>
+          <span>{this.state.result}</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
---
+++ src/helpers/converter.helper.js
@@ -0,0 +1,9 @@
+export function toRoman(val) {
+  console.log('to roman');
+  return val;
+}
+
+export function toInteger(val) {
+  console.log('to int');
+  return val;
+}
